714814be4b6539572c99d9615499e10282dbdd90
guiso.acelga <guiso.acelga@gmail.com>
2021-09-14 23:44:45 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ExampleComponent } from './example/example.component';
+import { AppRoutingModule } from './app.routing.module';
+import {MatCheckboxModule} from "@angular/material/checkbox";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
