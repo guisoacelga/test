821ff24d9eddeb15bdd53ec53a9b5eb0409fde78
violeta.cautin <violeta.cautin@probando.io>
2021-10-05 15:30:37 +0200

    Add Welcome Component

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.scss']
 })
 export class SidenavListComponent implements OnInit {
-
+  @Output() closeSideNav = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClose() {
+    this.closeSideNav.emit();
   }
 
 }
